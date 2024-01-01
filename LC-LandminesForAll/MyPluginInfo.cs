d2a1d9af8893c2a3098c4da244d009911438c0a4
Sakura Kade Hansen <137737608+SakuraKade@users.noreply.github.com>
2024-01-01 14:27:12 +0100

    Fixed version numbes

--- LC-LandminesForAll/MyPluginInfo.cs
+++ LC-LandminesForAll/MyPluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string PLUGIN_GUID = "sakura.lc.landminesforall";
         public const string PLUGIN_NAME = "LandminesForAll";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "1.0.1";
     }
 }
